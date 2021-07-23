65f6125030347ef9dc584676b1c297435db513a6
Shashank <shashankpateriya4@gmail.com>
2021-07-23 13:53:45 +0530

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(45,65,78);
+            Console.WriteLine(output);
+        }
+    }
+}
