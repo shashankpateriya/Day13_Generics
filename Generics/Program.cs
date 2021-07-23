dd6f6194cc527881b48dabc47e2b3c925bd7a78a
Shashank <shashankpateriya4@gmail.com>
2021-07-23 14:03:02 +0530

    [shashank] Add: Check maximum between three floats

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -9,6 +9,9 @@ namespace Generics
             Console.WriteLine("Find maximum Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(45,65,78);
             Console.WriteLine(output);
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(17.8, 24.67, 70.96);
+            Console.WriteLine(doubleoutput);
+
         }
     }
 }
